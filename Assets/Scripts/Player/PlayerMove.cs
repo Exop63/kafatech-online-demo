cbd58816d1d76427a64f94468fd3e014229a5b98
Exop63 <abdulahbaglan@gmail.com>
2024-05-28 12:49:19 +0300

    screen bound add for player

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,21 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using Mirror;
 using UnityEngine;
 
 public class PlayerMove : NetworkBehaviour
 {
     public float speed = 1;
-
+    public Vector2 bounds = Vector2.one;
 
     [SyncVar]
     [HideInInspector] public Vector2 direction;
 
     public bool IsMoving => direction.sqrMagnitude > 0;
+
+
+    private void Awake()
+    {
+        var characterBounds = GetComponentInChildren<SpriteRenderer>().bounds.size;
+        bounds = Utils.GetScreenBounds() - (Vector2)characterBounds * .5f;
+    }
     [Command]
     public void CmdSetDirectin(Vector2 dir)
     {
         direction = dir;
-        transform.position = Vector2.Lerp(transform.position, (Vector2)transform.position + dir.normalized, Time.deltaTime * speed);
+        transform.position = Vector2.Lerp(transform.position, Utils.ScreenClamp((Vector2)transform.position + dir.normalized, bounds), Time.deltaTime * speed);
+
     }
 }
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils
+{
+    public static Vector3 ScreenClamp(Vector3 position, Vector2 bounds)
+    {
+        position.x = Mathf.Clamp(position.x, -bounds.x, bounds.x);
+        position.y = Mathf.Clamp(position.y, -bounds.y, bounds.y);
+        return position;
+    }
+    public static Vector2 GetScreenBounds()
+    {
+        return GetScreenBounds(Camera.main);
+    }
+    public static Vector2 GetScreenBounds(Camera cam)
+    {
+        // Viewport'un sağ üst köşesini Dünya koordinatlarına dönüştür
+        Vector3 topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
+
+        // Yalnızca X (sağ sınır) ve Y (üst sınır) değerlerini al ve bir Vector2 olarak döndür
+        return new Vector2(topRight.x, topRight.y);
+    }
+
+}
